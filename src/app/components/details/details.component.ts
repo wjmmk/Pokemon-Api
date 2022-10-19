10d2e339926231028bda45769612696e7236f232
William Mosquera <wjmmk3@gmail.com>
2022-10-19 15:35:37 -0500

    DetailsComponent ~ Working

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ConsultarApiService } from 'src/app/services/consultar-api.service';
 import { pokemon }  from '../../shared/interface/pokemon.interface'
 
@@ -11,7 +11,6 @@ import { pokemon }  from '../../shared/interface/pokemon.interface'
 export class DetailsComponent implements OnInit {
 
   constructor( 
-    private router: Router,
     private route: ActivatedRoute,
     private listpokemon: ConsultarApiService 
     ) { }
@@ -24,8 +23,21 @@ export class DetailsComponent implements OnInit {
       if (params.has('id')) {
         this.listpokemon.getPokemonId(parseInt(params.get('id'))).subscribe(
           (result: any) => {
+            const pokemonCard = [{
+              id: result.id,
+              img: result.sprites.other.dream_world.front_default,
+              name: result.name,
+              ability: result.abilities.map((ability) => ability.ability.name),
+              attack: result.stats[1].base_stat,
+              speciality: result.stats[2].base_stat,
+              defency: result.stats[3].base_stat,
+              url: result.location_area_encounters
+            }]
+            console.log(pokemonCard)
+            this.pokeResult = pokemonCard
             this.listpokemon.getPokemonId(result.id).subscribe(
-              (data: any) => console.log(data)
+              (data: any) => {                  
+              }
             )
           }
         )
